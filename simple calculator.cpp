e324cbf61700e41d014e0cab5f7d73567d858e38
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2023-04-28 22:13:45 +0530

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    double p,q;
+    int a,b;
+    char operation,redo;
+ 
+     cout<<"Welcome to the calculater program v.1.0 written by Your Name"<<endl;
+    cout<<"***************************************************************"<<endl;
+    cout<<endl<<endl<<endl;
+   
+    do
+    {
+   	    cout<<" Please enter an operation which you like to calculate (+,-,*,/,m,p,s,r)";
+        cout<<"[s = swap]/[p = power of]/[m = mod]/[r = root of]:";
+        cin>>operation ;
+        cout<<endl<<endl;
+        if(operation == 'm')
+        {
+        	cout<<" Please enter two numbers to apply your requested operation(";
+        	cout<<operation<<"):"<<endl<<"1st num:";
+        	cin>>a;
+        	cout<<"2nd num:" ;
+        	cin>>b;
+        	cout<<endl;
+    	}
+    	else
+    	{
+    		cout<<" Please enter two numbers to apply your requested operation(";
+        	cout<<operation<<"):"<<endl<<"1st num:";
+       	 	cin>>p;
+       		cout<<"2nd num:" ;
+        	cin>>q;
+        	cout<<endl;
+		}
+
+        switch (operation)
+        {
+        case'+':
+            cout<<"The addition of two numbers ("<<p<<","<<q<<"):";
+            cout<<p+q<<endl;
+            break;
+        case'-':
+            cout<<"The substraction of two numbers ("<<p<<","<<q<<"):";
+            cout<<p-q<<endl;
+            break;
+        case'*':
+            cout<<"The multiplication of two numbers ("<<p<<","<<q<<"):";
+            cout<<p*q<<endl;
+            break;
+        case'/':
+            cout<<"The division of two numbers ("<<p<<","<<q<<"):";
+            if(q==0)
+            {
+                cout<<"Invalid"<<endl;
+            }
+            cout<<(p/q)<<endl;
+            break;
+        case'm':
+            cout<<"The modulo of two numbers ("<<a<<","<<b<<"):";
+            if(b==0)
+            {
+                cout<<"Invalid "<<endl;
+            }
+            cout<<a%b<<endl;
+            break;
+        case'p':
+            cout<<"The number"<<p<<" to the power of "<<q<<":";
+            cout<<pow(p,q)<<endl;
+            break;
+        case's':
+            cout<<"The swap of two numbers ("<<p<<","<<q<<"):";
+            swap(p,q);
+            cout<<"1stnumber="<<p<<"and 2nd number="<<q<<endl<<endl;
+            break;
+        case'r':
+            cout<<"The "<<q<<"th root of number "<<p<<":";
+            cout<<pow(p,(1/q))<<endl;
+            break;
+        default:
+            cout<<"unknown command"<<endl;
+
+        }
+     
+        cout<<"enter y or Y to continue:";
+        cin>>redo;
+        cout<<endl<<endl;
+    }
+    while(redo=='y'||redo=='Y');
+
+    return 0;
+
+}
+
